72b49ef9f319015dbbfea674b1bd674ac6092f24
tsv-stacks <tsvirdi@yahoo.co.uk>
2022-10-20 00:29:58 +0100

    added js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+const arabicArr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+const romanArr = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+let btn = document.getElementById("btn")
+let resultInput = document.getElementById("roman-num")
+let numberInput = document.getElementById("number-input")
+
+btn.addEventListener("click", () => conversionCheck())
+
+// Make sure this code gets executed after the DOM is loaded.
+//document.querySelector("#addLinks").addEventListener("keyup", event => {
+//if(event.key !== "Enter") return; // Use `.key` instead.
+//document.querySelector("#linkadd").click(); // Things you want to do.
+//event.preventDefault(); // No need to `return false;`.
+//});
+
+function conversionCheck() {
+    let num = parseInt(numberInput.value)
+    if (num < 1 || num > 10000 || "") {
+        alert("Please enter a number between 1 and 10000!")
+        numberInput.value = ""
+    } else {
+        let result = convertToRoman(num)
+        resultInput.innerText = result
+    }
+}
+
 function convertToRoman(num) {
-    const arabicArr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    const romanArr = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     let arabicArr2 = []
     let arabicArr3 = []
     let romanArr2 = []
@@ -15,20 +39,14 @@ function convertToRoman(num) {
             arabicArr3.push("0")
         }
     }
-
     for (let i = 0; i < arabicArr3.length; i++) {
         if (arabicArr3[i] != 0) {
             let a = arabicArr3[i]
             let b = romanArr[i]
             let c = b.repeat(a)
             romanArr2.push(c)
-            console.log(c)
         }
     }
-
-    console.log("arr2: " + arabicArr2)
-    console.log("arr3: " + arabicArr3)
+    // console.log(romanArr2.join(''))
     return romanArr2.join('')
 }
-
-convertToRoman(36);
